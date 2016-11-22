09dcf8d11988d77f07eaf3c4cd11dfb17d9092bb
Hal DeLage <hal.delage@teamaol.com>
2016-11-22 15:18:24 -0500

    Add content to about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,10 +1,31 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var About = () => {
     return (
         <div>
-            <h3>About</h3>
-            <p>welcome to about page</p>
+            <h1 className="text-center">About</h1>
+            <p>This is a Weather Application built with React. I built this for the Complete React 
+            Web App Developer course.</p>
+            <p>Powered By:</p>
+            <ul>
+                <li>
+                    <a href="https://facebook.github.io/react/">React</a> - This was the javascript 
+                    framework used.
+                </li>
+                <li>
+                    <a href="http://foundation.zurb.com/">Foundation</a> - This is the CSS framework used.
+                </li>
+                <li>
+                    <a href="http://openweathermap.org/">Open Weather Map</a> - I used Open Weather 
+                    Map to search for weather data by city name.
+                </li>
+                <li>
+                    <a href="https://github.com/halliej/react-weather">Project page on github</a> - My source 
+                    code.
+                </li>
+            </ul>
+
         </div>
     )
 };
