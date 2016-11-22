928d1cbcdcc201c7c82ac852d03131a1e7011a21
Hal DeLage <hal.delage@teamaol.com>
2016-11-22 14:55:15 -0500

    Updated examples page, added main styles

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = () => {
     return (
         <div>
-            <h3>Examples</h3>
-            <p>welcome to examples page</p>
+            <h1 className="text-center">Examples</h1>
+            <p>Here are a few example locations to try out:</p>
+            <ol>
+                <li>
+                    <Link to="/?location=Gardners, PA">Gardners, PA</Link>
+                </li>
+                <li>
+                    <Link to="/?location=Baton Rouge, LA">Baton Rouge, LA</Link>
+                </li>
+            </ol>
         </div>
     )
 };
